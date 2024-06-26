d3c9083f947feb4ed362146e96c350448cdfa1cc
PedroItaloQ <peedroitalo2906@gmail.com>
2024-06-25 21:48:46 -0300

    feat: created import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+gitINSERT INTO sector(name) VALUES ('Gestão');
+INSERT INTO sector(name) VALUES ('Informática');
+
+INSERT INTO employee(sector_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO employee(sector_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO employee(sector_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO employee(sector_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
