3d5af50038783c7cd967386a2913827773249882
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-09 20:56:42 -0300

    feat: created sectorEmployeeTest

---
+++ src/test/java/com/dev/sectoremployee/SectoremployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sectoremployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectoremployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
