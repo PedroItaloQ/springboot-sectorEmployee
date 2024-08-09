6f8fec94c1a9d6bcf7d9c0f4e9aaca686d6810d6
PedroItaloQ <peedroitalo2906@gmail.com>
2024-08-09 13:43:01 -0300

    feat: added sectoremployee

---
+++ src/main/java/com/dev/sectoremployee/SectoremployeeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sectoremployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectoremployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectoremployeeApplication.class, args);
+	}
+
+}
